5d0ed377ef8c3aecdd214986037b1b521cf618ed
quang200301 <quang.a24@student.passerellesnumeriques.org>
2023-03-29 19:26:20 +0700

    css

--- LoginLogout/sigup.php
+++ LoginLogout/sigup.php
@@ -3,31 +3,63 @@ include 'connect.php';
  ?>
 <!DOCTYPE html>
 <html lang="en">
+    <link rel="stylesheet" href="style.css">
 <head>
-    <style >
-        
-    </style>
- 
-    <title>Thêm</title>
-</head>
-<body>
-    <form action="" method="post">
-        <label for="">Name</label>
-        <input type="text" name="name" id="name"><br>
-  
-        <label for="">Password</label>
-        <input type="text" name="pass" id="pass"> <br>
-        <label for="">Email</label>
-        <input type="text" name="email" id="email"><br>
-        <label for="">Phone</label>
-        <input type="text" name="phone" id="phone"><br>
-        <label for="">Dist</label>
-        <input type="text" name="dist" id="dist"><br>
 
-        <label for="">City</label>
-        <input type="text" name="city"><br>
-        <input type="submit" name="subm" value="Thêm">
-    </form>
+</head>
+
+<body>
+    <div class="main">
+        <div class="allconten">
+            <div class="contener">
+                <div class="div1">
+                    <button type="button"  class="my" onclick="myk()">Sign In</button>
+                    <script>
+                        function myk() {
+                            location.replace("sigin.php");
+                        }
+                    </script>
+                </div>
+                <div class="div2">
+                <button type="button" class="my_su" onclick="myFunction()">Sign Up</button>
+                <script>
+                    function myFunction() {
+                        location.replace("sigup.php");
+                    }
+                </script>  
+                </div>
+            </div>
+            <div class="conten">
+                <div class="wrapp">        
+                    <div class="left-conten">     
+                        <form action="" method="post">
+                            <input type="text" name="name" id="name" placeholder="Name"><br><br>
+                            <input type="text" name="pass" id="pass" placeholder="Password"> <br><br>
+                            <div class="e_p">
+                            <input type="text" name="email" id="email" placeholder="Email">
+                            <input type="text" name="phone" id="phone"placeholder="Phone">
+                            </div><br>
+                            <div class="d_c">
+                            <input type="text" name="dist" id="dist"placeholder="Dist">
+                            <input type="text" name="city" id="city"placeholder="City">
+                            </div><br>
+                            <input type="submit" name="subm" id="sm"value="Sign Up">
+                        </form> 
+                    </div>
+                    <div class="right-conten">
+                        <div id="p1">Being a member of</div>
+                        <div class="pp"> 
+                            <div id="mkey">MAKEIT</div>
+                            <div id="men">Men to explore</div>
+                        </div>
+                        <div id="p3"> your own style.</div>
+                    </div>
+                </div>
+                    <p id="p1"> You don't have an account yes? <a  class="a1"href="signup.php">Sign up</a></p>
+                </div>
+            </div>
+        </div>
+</body>
     <?php
     if(isset($_POST['subm'])){
         $name=$_POST['name'];
@@ -41,7 +73,4 @@ include 'connect.php';
             echo"<script> alert('successfuly')</script>";
         }
     }
-    ?>
-    
-</body>
-</html>
+?>   
