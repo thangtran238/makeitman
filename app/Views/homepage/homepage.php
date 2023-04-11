19c69f96e34910cd324cee3ce84e122ca57f8a08
Huong141709 <118873328+Huong141709@users.noreply.github.com>
2023-04-11 08:58:59 +0700

    homepage chuẩn bị in ảnh

---
+++ app/Views/homepage/homepage.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+    <link rel="stylesheet" href="homepage.css">
+    <script src="https://code.jquery.com/jquery-3.6.3.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script></head>
+<body>
+    <div id="interface">
+        <div class="navigation">
+            <div class="logo">
+                <h3><span class="blue">MAKEIT</span><span class="white">Men.</span></h3>
+            </div>
+            <div class="input-group">
+                <input id="input" type="text">
+                        <span class="input-group-text">
+                        <i class="fa-solid fa-search"></i>
+                    </span>
+            </div>
+            <div class="form-container">
+                <div class="slide-controls">
+                    <label for="login" class="slide-signin" href="#" >Sign In</label>
+                </div>
+                <div class="slide-controls">
+                    <label for="login" class="slide-signup" href="#">Sign Up</label>
+                </div>
+            </div>
+        </div> 
+    </div>
+    <div class = "row">
+        <div class = "col-sm-2">
+            <div class="items">
+                <li><i class="fa-solid fa-hashtag"></i><a href="#">All</a></li>
+                <li><i class="fa-solid fa-hashtag"></i> <a href="#">T-Shirt</a></li>
+                <li><i class="fa-solid fa-hashtag"></i><a href="#">Trousers</a></li>
+                <li><i class="fa-solid fa-hashtag"></i><a href="#">Sneakers</a></li>
+                <li><i class="fa-solid fa-hashtag"></i><a href="#">Accessory</a></li>
+                <li><i class="fa-solid fa-hashtag"></i><a href="#">Underwear</a></li>
+            </div>
+        </div>
+        <div class = "col-sm-10">
+            <div><h1> The <span class="best-option">Best Option</span> For The Your Look!</h1></div>
+            <div><h5>In here, we bring you comfortable, outshine products!</h5></div>
+            <div><h5>Fllow us on <i class="fa-brands fa-facebook"></i> <i class="fa-brands fa-instagram"></i> <i class="fa-brands fa-twitter"></i></h5></div>
+            <div class="row">
+                <div class="col-sm-3">
+                     <img src="/makeitman/public/img/sneakers.png" alt="Hình ảnh"> 
+                     <i class="fa-solid fa-heart"></i>
+                      <i class="fa-solid fa-cart-plus"></i>
+                </div>
+                <div class="col-sm-3"> <img src="../../../public/img/sneakers.png" alt="Hình ảnh"></div>
+                <div class="col-sm-3"> <img src="https://i.pinimg.com/236x/17/63/d7/1763d7026d3edf9140f61ffa86348054.jpg" alt="Hình ảnh"></div>
+                <div class="col-sm-3"> <img src="https://i.pinimg.com/236x/17/63/d7/1763d7026d3edf9140f61ffa86348054.jpg" alt="Hình ảnh"></div>
+            </div>
+        </div>
+        
+</div>
+    </div>
+    </div> 
+    
+
+    </div>
+</body>
+</html>
